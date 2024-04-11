05477fa6324547364494cd1988b3431954bbe7c3
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2024-04-10 22:27:32 -0300

    commit

--- app.R
+++ app.R
@@ -8,15 +8,62 @@
 #
 
 library(shiny)
+library(ggplot2)
+data(penguins, package = "palmerpenguins")
 
-# Define UI for application that draws a histogram
-ui <- 
+cards <- list(
+  card(
+    full_screen = TRUE,
+    card_header("Sem Filtro"),
+    plotOutput("bill_length")
+  ),
+  card(
+    full_screen = TRUE,
+    card_header("Sexo"),
+    plotOutput("bill_depth")
+  ),
+  card(
+    full_screen = TRUE,
+    card_header("Faixa Etária"),
+    plotOutput("body_mass")
+  ),
+  card(
+    full_screen = TRUE,
+    card_header('Ambos')
+    )
+)
 
-# Define server logic required to draw a histogram
+color_by <- varSelectInput(
+  "color_by", "Color by",
+  penguins[c("species", "island", "sex")],
+  selected = "species"
+)
+
+ui <- page_sidebar(
+  title = "Penguins dashboard",
+  sidebar = color_by,
+  navset_card_underline(
+    title = "Histograms by species",
+    nav_panel('oi', cards[[1]]),
+    nav_panel('a', cards[[2]]),
+    nav_panel('b', cards[[3]]),
+    nav_panel('c', cards[[4]])
+  )
+)
+
+
+
 server <- function(input, output) {
-
-    
+  gg_plot <- reactive({
+    ggplot(penguins) +
+      geom_density(aes(fill = !!input$color_by), alpha = 0.2) +
+      theme_bw(base_size = 16) +
+      theme(axis.title = element_blank())
+  })
+  
+  output$bill_length <- renderPlot(gg_plot() + aes(bill_length_mm))
+  output$bill_depth <- renderPlot(gg_plot() + aes(bill_depth_mm))
+  output$body_mass <- renderPlot(gg_plot() + aes(body_mass_g))
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
